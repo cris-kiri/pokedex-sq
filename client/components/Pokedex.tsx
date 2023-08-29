fa0e369b9e6dcca6e02ef7c7559cd3ef757d8256
Cris Kiri <cris.kiri@gmail.com>
2023-08-29 23:48:59 +1200

    feat: add query to fetch pokemon data by generation

--- client/components/Pokedex.tsx
+++ client/components/Pokedex.tsx
@@ -1,12 +1,34 @@
 import { useState, useEffect } from 'react'
 
 import Pokemon from './Pokemon'
+import { fetchPokemonByGeneration } from '../apis/pokeapi'
+import { useQuery } from '@tanstack/react-query'
+import { Link } from 'react-router-dom'
 
 export default function Pokedex() {
+  const {
+    data: generation,
+    isLoading,
+    isError,
+  } = useQuery(['pokemon'], async () => await fetchPokemonByGeneration(1))
+
+  console.log(generation)
+
+  if (isLoading) return <div>Loading...</div>
+
+  if (isError) return <div>There was an error!</div>
+
   return (
     <>
       <h1>Gotta catch &apos;em all...</h1>
-      <Pokemon />
+
+      <ul>
+        {generation.pokemon_species.map((p, index: number) => (
+          <li key={index}>
+            <Link to={`/pokemon/${p.name}`}>{p.name}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
