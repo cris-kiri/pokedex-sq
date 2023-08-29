74e4c9e39cc40293919485808c5d16fa1e55935e
Cris Kiri <cris.kiri@gmail.com>
2023-08-29 23:54:58 +1200

    feat: add query client provider wrapper

--- client/index.tsx
+++ client/index.tsx
@@ -3,9 +3,14 @@ import { RouterProvider } from 'react-router-dom'
 
 import App from './components/App.tsx'
 import { router } from './router.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   )
 })
