81a26fc8b32308778c31766d24de0f00ef44f8c9
Cris Kiri <cris.kiri@gmail.com>
2023-09-02 15:13:35 +1200

    feat: select from generations 1, 2, or 3 and update pokemon list

--- client/components/Pokedex.tsx
+++ client/components/Pokedex.tsx
@@ -1,34 +1,62 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 
 import Pokemon from './Pokemon'
 import { fetchPokemonByGeneration } from '../apis/pokeapi'
-import { useQuery } from '@tanstack/react-query'
-import { Link } from 'react-router-dom'
+import { Species } from '../../models/pokemon'
+// import { Pokemon as PokemonType } from '../../models/pokemon'
 
 export default function Pokedex() {
+  const [gen, setGen] = useState('1')
+  const [pokemon, setPokemon] = useState('bulbasaur')
   const {
     data: generation,
     isLoading,
     isError,
-  } = useQuery(['pokemon'], async () => await fetchPokemonByGeneration(1))
-
-  console.log(generation)
+  } = useQuery(
+    ['generation', gen],
+    async () => await fetchPokemonByGeneration(Number(gen))
+  )
 
   if (isLoading) return <div>Loading...</div>
 
   if (isError) return <div>There was an error!</div>
 
+  function handleGenChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    setGen(event.target.value)
+  }
+
+  function handlePokemonChange(event: React.ChangeEvent<HTMLSelectElement>) {
+    setPokemon(event.target.value)
+  }
+
   return (
     <>
       <h1>Gotta catch &apos;em all...</h1>
+      <select
+        value={gen}
+        name="generation"
+        id="generation"
+        onChange={handleGenChange}
+      >
+        <option value="1">Gen 1</option>
+        <option value="2">Gen 2</option>
+        <option value="3">Gen 3</option>
+      </select>
 
-      <ul>
-        {generation.pokemon_species.map((p, index: number) => (
-          <li key={index}>
-            <Link to={`/pokemon/${p.name}`}>{p.name}</Link>
-          </li>
+      <select
+        value={pokemon}
+        name="pokemon"
+        id="pokemon"
+        onChange={handlePokemonChange}
+      >
+        {generation?.pokemon_species.map((pokemon: Species) => (
+          <option key={pokemon.name} value={pokemon.name}>
+            {pokemon.name}
+          </option>
         ))}
-      </ul>
+      </select>
+      <Pokemon name={pokemon} />
     </>
   )
 }
