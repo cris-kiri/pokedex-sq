b94f80da4b2af50aabe2e5a681c3fda713c16fc8
Cris Kiri <cris.kiri@gmail.com>
2023-09-02 21:56:31 +1200

    refactor: update get pokemon by generation to use endpoint based off pokemon order

--- client/apis/pokeapi.ts
+++ client/apis/pokeapi.ts
@@ -1,11 +1,32 @@
 import request from 'superagent'
 
-export async function fetchPokemonByGeneration(gen: number) {
-  const response = await request.get(
-    `https://pokeapi.co/api/v2/generation/${gen}`
-  )
+// export async function fetchPokemonByGeneration(gen: number) {
+//   const response = await request.get(
+//     `https://pokeapi.co/api/v2/generation/${gen}`
+//   )
 
-  return response.body
+//   return response.body
+// }
+
+export async function fetchPokemonByGeneration(gen: number) {
+  if (gen === 1) {
+    const response = await request.get(
+      'https://pokeapi.co/api/v2/pokemon?limit=151'
+    )
+    return response.body
+  }
+  if (gen === 2) {
+    const response = await request.get(
+      'https://pokeapi.co/api/v2/pokemon?limit=100&offset=151'
+    )
+    return response.body
+  }
+  if (gen === 3) {
+    const response = await request.get(
+      'https://pokeapi.co/api/v2/pokemon?limit=135&offset=251'
+    )
+    return response.body
+  }
 }
 
 export async function fetchPokemonByName(name: string) {
