1325e9dd173d6f7600b282a8466e61ac9ec7fbef
Cris Kiri <cris.kiri@gmail.com>
2023-09-03 15:43:18 +1200

    chore: display stats/info in tabular form

--- client/components/Pokemon.tsx
+++ client/components/Pokemon.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 
 import { fetchPokemonByName } from '../apis/pokeapi'
-import { type Pokemon } from '../../models/pokemon'
+import { type Pokemon, Species } from '../../models/pokemon'
 import LoadingPokeball from './LoadingPokeball'
 interface Props {
   name: string
@@ -27,28 +27,64 @@ export default function Pokemon(props: Props) {
           {pokemon?.name.toUpperCase()} #{pokemon?.id}
         </h2>
       </div>
-      <div>
-        <div className="pokemon-body">
-          <img
-            src={pokemon?.sprites.front_default}
-            alt={pokemon?.name}
-            className="pokemon-image"
-          />
-          <div className="pokemon-stats">
-            <div className="info">
-              <p>HEIGHT: {pokemon?.height}</p>
-              <p>WEIGHT: {pokemon?.weight}</p>
-              {pokemon?.types.map((type) => (
-                <p key={type.type.name}>{type.type.name.toUpperCase()}</p>
-              ))}
-            </div>
-            <div className="stats">
-              {pokemon?.stats.map((stat) => (
+      <div className="pokemon-body">
+        <img
+          src={pokemon?.sprites.front_default}
+          alt={pokemon?.name}
+          className="pokemon-image"
+        />
+        <div className="pokemon-stats">
+          <div className="info">
+            <table>
+              <tr>
+                <td>
+                  <h4>HEIGHT:</h4> {pokemon?.height}
+                </td>
+                <td>
+                  <h4>WEIGHT:</h4> {pokemon?.weight}
+                </td>
+              </tr>
+              <tr>
+                {pokemon?.types.map((type) => (
+                  <td key={type.type.name}>
+                    <h4>{type.type.name.toUpperCase()}</h4>
+                  </td>
+                ))}
+              </tr>
+            </table>
+          </div>
+          <div className="stats">
+            <table>
+              <tr>
+                <td>
+                  <h4>HP:</h4> {pokemon?.stats[0].base_stat}
+                </td>
+                <td>
+                  <h4>SPEED:</h4> {pokemon?.stats[5].base_stat}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h4>ATTACK:</h4> {pokemon?.stats[1].base_stat}
+                </td>
+                <td>
+                  <h4>SP ATTACK:</h4> {pokemon?.stats[3].base_stat}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <h4>DEFENSE:</h4> {pokemon?.stats[2].base_stat}
+                </td>
+                <td>
+                  <h4>SP DEFENSE:</h4> {pokemon?.stats[4].base_stat}
+                </td>
+              </tr>
+            </table>
+            {/* {pokemon?.stats.map((stat) => (
                 <p key={stat.stat.name}>
                   {stat.stat.name.toUpperCase()}: {stat.base_stat}
                 </p>
-              ))}
-            </div>
+              ))} */}
           </div>
         </div>
       </div>
