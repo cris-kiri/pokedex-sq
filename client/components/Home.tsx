c77f4c0eb7a611a0607fe507156c515a281ebd51
Cris Kiri <cris.kiri@gmail.com>
2023-09-09 14:48:08 +1200

    refactor: remove inline styling and add css class

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -2,17 +2,7 @@ export default function Home() {
   return (
     <>
       <a href="/pokedex">
-        <img
-          src="/images/pokedex.png"
-          alt="pokedex"
-          style={{
-            display: 'block',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            width: '300px',
-            height: 'auto',
-          }}
-        />
+        <img src="/images/pokedex.png" alt="pokedex" className="home" />
       </a>
     </>
   )
