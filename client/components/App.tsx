4cc6cbc2df36efd6b53d6b2c53eee4f75b0987f3
Cris Kiri <cris.kiri@gmail.com>
2023-09-09 14:47:00 +1200

    refactor: separate logos to component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,35 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import Logos from './Logos'
 
-const App = () => {
+export default function App() {
   return (
     <>
-      <a href="/">
-        <img
-          src="/images/pokemon-logo.png"
-          alt="pokemon logo"
-          style={{
-            display: 'block',
-            marginLeft: 'auto',
-            marginRight: 'auto',
-            width: '350px',
-            height: 'auto',
-          }}
-        />
-      </a>
-      <img
-        src="/images/pokedex-logo.png"
-        alt="pokedex logo"
-        style={{
-          display: 'block',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          width: '300px',
-          height: 'auto',
-        }}
-      />
+      <Logos />
       <Outlet />
     </>
   )
 }
-
-export default App
