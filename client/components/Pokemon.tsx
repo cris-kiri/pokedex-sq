cc8a8b9f4790bb4921c83f1aec6276142a6b4c16
Cris Kiri <cris.kiri@gmail.com>
2023-09-10 00:33:26 +1200

    refactor: info and stats now using flexbox and grid

--- client/components/Pokemon.tsx
+++ client/components/Pokemon.tsx
@@ -3,12 +3,12 @@ import { useQuery } from '@tanstack/react-query'
 import { fetchPokemonByName } from '../apis/pokeapi'
 import { type Pokemon } from '../../models/pokemon'
 import LoadingPokeball from './LoadingPokeball'
-import Stat from './Stat'
 interface Props {
   name: string
 }
 
 export default function Pokemon({ name }: Props) {
+  const DEFAULT_STAT = 1
   const {
     data: pokemon,
     error,
@@ -19,13 +19,12 @@ export default function Pokemon({ name }: Props) {
 
   if (isLoading) return <LoadingPokeball />
 
+  // This needs breaking down into smaller components with props
   return (
     <>
-      <div id="pokemon-header">
-        <h2>
-          {pokemon?.name.toUpperCase()} #{pokemon?.id}
-        </h2>
-      </div>
+      <h2>
+        {pokemon?.name.toUpperCase()} #{pokemon?.id}
+      </h2>
       <div className="pokemon-body">
         <img
           src={pokemon?.sprites.front_default}
@@ -33,71 +32,65 @@ export default function Pokemon({ name }: Props) {
           className="pokemon-image"
         />
         <div className="pokemon-stats">
-          <div className="info">
-            <table>
-              <tbody>
-                <tr>
-                  {pokemon?.types.map((type) => (
-                    <td key={type.type.name}>
-                      <Stat title={type.type.name} stat={0} />
-                    </td>
-                  ))}
-                </tr>
-                <tr>
-                  <td>
-                    <Stat title="height" stat={pokemon?.height || 1} />
-                  </td>
-                  <td>
-                    <Stat title="weight" stat={pokemon?.weight || 1} />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+          <div className="stats">
+            <div className="grid-container-info">
+              {pokemon?.types.map((type, index) => (
+                <div
+                  className={`grid-item-type-${index + 1}`}
+                  key={type.type.name}
+                >
+                  <h4>{type.type.name}</h4>
+                </div>
+              ))}
+              <div className="grid-item-height-title">
+                <h4>Height</h4>
+              </div>
+              <div className="grid-item-height-value">{pokemon?.height}</div>
+              <div className="grid-item-weight-title">
+                <h4>Weight</h4>
+              </div>
+              <div className="grid-item-weight-value">{pokemon?.weight}</div>
+            </div>
           </div>
           <div className="stats">
-            <table>
-              <tbody>
-                <tr>
-                  <td>
-                    <Stat title="hp" stat={pokemon?.stats[0].base_stat || 1} />
-                  </td>
-                  <td>
-                    <Stat
-                      title="speed"
-                      stat={pokemon?.stats[5].base_stat || 1}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <Stat
-                      title="attack"
-                      stat={pokemon?.stats[1].base_stat || 1}
-                    />
-                  </td>
-                  <td>
-                    <Stat
-                      title="sp attack"
-                      stat={pokemon?.stats[3].base_stat || 1}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <Stat
-                      title="defense"
-                      stat={pokemon?.stats[2].base_stat || 1}
-                    />
-                  </td>
-                  <td>
-                    <Stat
-                      title="sp defense"
-                      stat={pokemon?.stats[4].base_stat || 1}
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div className="grid-container-stats">
+              <div className="grid-item-stat-1-title">
+                <h4>HP</h4>
+              </div>
+              <div className="grid-item-stat-1-value">
+                {pokemon?.stats[0].base_stat || DEFAULT_STAT}
+              </div>
+              <div className="grid-item-stat-2-title">
+                <h4>SPEED</h4>
+              </div>
+              <div className="grid-item-stat-2-value">
+                {pokemon?.stats[5].base_stat || DEFAULT_STAT}
+              </div>
+              <div className="grid-item-stat-3-title">
+                <h4>ATTACK</h4>
+              </div>
+              <div className="grid-item-stat-3-value">
+                {pokemon?.stats[1].base_stat || DEFAULT_STAT}
+              </div>
+              <div className="grid-item-stat-4-title">
+                <h4>SP ATTACK</h4>
+              </div>
+              <div className="grid-item-stat-4-value">
+                {pokemon?.stats[3].base_stat || DEFAULT_STAT}
+              </div>
+              <div className="grid-item-stat-5-title">
+                <h4>DEFENSE</h4>
+              </div>
+              <div className="grid-item-stat-5-value">
+                {pokemon?.stats[2].base_stat || DEFAULT_STAT}
+              </div>
+              <div className="grid-item-stat-6-title">
+                <h4>SP DEFENSE</h4>
+              </div>
+              <div className="grid-item-stat-6-value">
+                {pokemon?.stats[4].base_stat || DEFAULT_STAT}
+              </div>
+            </div>
           </div>
         </div>
       </div>
